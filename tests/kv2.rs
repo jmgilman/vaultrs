fe55afadda7a9085462875fa012213fd38c4e7e4
Joshua Gilman <joshuagilman@gmail.com>
2021-08-18 11:42:24 -0700

    Combines testing of kv2 engine to reduce number of times containers are being created

--- tests/kv2.rs
+++ tests/kv2.rs
@@ -7,11 +7,38 @@ use vaultrs::error::ClientError;
 use vaultrs::kv2;
 
 #[test]
-fn test_delete_latest() {
+fn test() {
     let docker = testcontainers::clients::Cli::default();
     let server = VaultServer::new(&docker);
     let endpoint = setup(&server).unwrap();
 
+    // Test set / read
+    test_list(&server, &endpoint);
+    test_read(&server, &endpoint);
+    test_read_metadata(&server, &endpoint);
+    test_read_version(&server, &endpoint);
+    test_set(&server, &endpoint);
+    test_set_metadata(&server, &endpoint);
+
+    // Test delete
+    test_delete_latest(&server, &endpoint);
+    test_undelete_versions(&server, &endpoint);
+
+    test_delete_versions(&server, &endpoint);
+    create(&server, &endpoint).unwrap();
+
+    test_destroy_versions(&server, &endpoint);
+    create(&server, &endpoint).unwrap();
+
+    test_delete_metadata(&server, &endpoint);
+    create(&server, &endpoint).unwrap();
+
+    // Test config
+    crate::config::test_set(&server, &endpoint);
+    crate::config::test_read(&server, &endpoint);
+}
+
+fn test_delete_latest(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::delete_latest(
         &server.client,
         endpoint.path.as_str(),
@@ -20,12 +47,7 @@ fn test_delete_latest() {
     assert!(res.is_ok());
 }
 
-#[test]
-fn test_delete_metadata() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_delete_metadata(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::delete_metadata(
         &server.client,
         endpoint.path.as_str(),
@@ -34,12 +56,7 @@ fn test_delete_metadata() {
     assert!(res.is_ok());
 }
 
-#[test]
-fn test_delete_versions() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_delete_versions(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::delete_versions(
         &server.client,
         endpoint.path.as_str(),
@@ -49,12 +66,7 @@ fn test_delete_versions() {
     assert!(res.is_ok());
 }
 
-#[test]
-fn test_destroy_versions() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_destroy_versions(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::destroy_versions(
         &server.client,
         endpoint.path.as_str(),
@@ -64,42 +76,19 @@ fn test_destroy_versions() {
     assert!(res.is_ok());
 }
 
-#[test]
-fn test_list() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_list(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::list(&server.client, endpoint.path.as_str(), "");
     assert!(res.is_ok());
     assert!(!res.unwrap().is_empty());
 }
 
-#[test]
-fn test_read() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
-    let res = kv2::set(
-        &server.client,
-        endpoint.path.as_str(),
-        "test",
-        &endpoint.secret,
-    );
-    assert!(res.is_ok());
-
+fn test_read(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::read::<TestSecret>(&server.client, endpoint.path.as_str(), "test");
     assert!(res.is_ok());
     assert_eq!(res.unwrap().key, endpoint.secret.key);
 }
 
-#[test]
-fn test_read_metadata() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_read_metadata(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::read_metadata(
         &server.client,
         endpoint.path.as_str(),
@@ -109,41 +98,13 @@ fn test_read_metadata() {
     assert!(!res.unwrap().versions.is_empty());
 }
 
-#[test]
-fn test_read_version() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let mut endpoint = setup(&server).unwrap();
-
-    let res = kv2::set(
-        &server.client,
-        endpoint.path.as_str(),
-        "test",
-        &endpoint.secret,
-    );
-    assert!(res.is_ok());
-
-    let old_value = endpoint.secret.key.clone();
-    endpoint.secret.key = "newkey".to_string();
-    let res = kv2::set(
-        &server.client,
-        endpoint.path.as_str(),
-        "test",
-        &endpoint.secret,
-    );
-    assert!(res.is_ok());
-
+fn test_read_version(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::read_version::<TestSecret>(&server.client, endpoint.path.as_str(), "test", 1);
     assert!(res.is_ok());
-    assert_eq!(res.unwrap().key, old_value);
+    assert_eq!(res.unwrap().key, endpoint.secret.key);
 }
 
-#[test]
-fn test_set() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_set(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::set(
         &server.client,
         endpoint.path.as_str(),
@@ -153,35 +114,17 @@ fn test_set() {
     assert!(res.is_ok());
 }
 
-#[test]
-fn test_set_metadata() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
+fn test_set_metadata(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::set_metadata(
         &server.client,
         endpoint.path.as_str(),
         endpoint.name.as_str(),
-        Some(SetSecretMetadataRequest::builder().cas_required(true)),
+        Some(SetSecretMetadataRequest::builder().delete_version_after("1h")),
     );
     assert!(res.is_ok());
 }
 
-#[test]
-fn test_undelete_versions() {
-    let docker = testcontainers::clients::Cli::default();
-    let server = VaultServer::new(&docker);
-    let endpoint = setup(&server).unwrap();
-
-    let res = kv2::delete_versions(
-        &server.client,
-        endpoint.path.as_str(),
-        endpoint.name.as_str(),
-        vec![1],
-    );
-    assert!(res.is_ok());
-
+fn test_undelete_versions(server: &VaultServer, endpoint: &SecretEndpoint) {
     let res = kv2::undelete_versions(
         &server.client,
         endpoint.path.as_str(),
@@ -192,27 +135,16 @@ fn test_undelete_versions() {
 }
 
 mod config {
-    use crate::{setup, VaultServer};
+    use crate::{SecretEndpoint, VaultServer};
     use vaultrs::{api::kv2::requests::SetConfigurationRequest, kv2::config};
 
-    #[test]
-    fn test_read() {
-        let docker = testcontainers::clients::Cli::default();
-        let server = VaultServer::new(&docker);
-        let endpoint = setup(&server).unwrap();
-
+    pub fn test_read(server: &VaultServer, endpoint: &SecretEndpoint) {
         let resp = config::read(&server.client, endpoint.path.as_str());
 
         assert!(resp.is_ok());
-        assert_eq!(resp.unwrap().max_versions, 0);
     }
 
-    #[test]
-    fn test_set() {
-        let docker = testcontainers::clients::Cli::default();
-        let server = VaultServer::new(&docker);
-        let endpoint = setup(&server).unwrap();
-
+    pub fn test_set(server: &VaultServer, endpoint: &SecretEndpoint) {
         let versions: u64 = 100;
         let resp = config::set(
             &server.client,
@@ -229,16 +161,26 @@ mod config {
 }
 
 #[derive(Debug)]
-struct SecretEndpoint {
+pub struct SecretEndpoint {
     pub path: String,
     pub name: String,
     pub secret: TestSecret,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct TestSecret {
+pub struct TestSecret {
     key: String,
     password: String,
+}
+
+fn create(server: &VaultServer, endpoint: &SecretEndpoint) -> Result<(), ClientError> {
+    kv2::set(
+        &server.client,
+        endpoint.path.as_str(),
+        endpoint.name.as_str(),
+        &endpoint.secret,
+    )?;
+    Ok(())
 }
 
 fn setup(server: &VaultServer) -> Result<SecretEndpoint, ClientError> {
@@ -258,12 +200,7 @@ fn setup(server: &VaultServer) -> Result<SecretEndpoint, ClientError> {
     server.mount(path, "kv-v2")?;
 
     // Create a test secret
-    kv2::set(
-        &server.client,
-        endpoint.path.as_str(),
-        endpoint.name.as_str(),
-        &endpoint.secret,
-    )?;
+    create(server, &endpoint)?;
 
     Ok(endpoint)
 }
