48ca29817fc81e4507296c5dd64529b60943ae6e
Luc Perkins <lucperkins@gmail.com>
2024-06-19 12:26:28 -0700

    Add request for creating/updating token roles

--- src/api/identity/identity_tokens/requests.rs
+++ src/api/identity/identity_tokens/requests.rs
@@ -1,6 +1,30 @@
 use super::responses::GenerateSignedIdTokenResponse;
 use rustify_derive::Endpoint;
 
+/// ## Create or update a role
+/// Create or update a role. ID tokens are generated against a role and signed against a named key.
+///
+/// * Path: /identity/oidc/role/{self.name}
+/// * Method: POST
+/// * Response: N/A
+/// * Reference: <https://developer.hashicorp.com/vault/api-docs/secret/identity/tokens#create-or-update-a-role>
+#[derive(Builder, Debug, Endpoint)]
+#[endpoint(
+    path = "/identity/oidc/role/{self.name}",
+    method = "POST",
+    builder = "true"
+)]
+#[builder(setter(into))]
+pub struct CreateOrUpdateTokenRoleRequest {
+    /// The name of the role to create
+    #[endpoint(skip)]
+    pub name: String,
+    pub key: String,
+    pub ttl: String,
+    pub template: Option<String>,
+    pub client_id: Option<String>,
+}
+
 /// ## Generate a signed token ID
 /// This endpoint generates a signed ID (OIDC) token.
 ///
